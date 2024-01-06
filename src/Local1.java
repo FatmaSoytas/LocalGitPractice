25549e99e8d802652fe24fd9b295bf274e717472
Fatma SOYTAS <fatmasoytas81@homail.com>
2024-01-06 20:06:50 +0100

    Lokal1 eski testler guncellendi

--- src/Local1.java
+++ src/Local1.java
@@ -1,6 +1,6 @@
 public class Local1 {
     public static void main(String[] args) {
-        System.out.println("Git Test");
+        System.out.println("Git Testi Fatma branch in da degistirdim");
         System.out.println("gityeni satir eklendi!");
         System.out.println("yeni test");
         System.out.println("cuma");
