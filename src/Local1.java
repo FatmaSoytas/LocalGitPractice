9e8b89a616064c20815672a49d07895d34245638
Fatma SOYTAS <fatmasoytas81@homail.com>
2024-01-06 18:12:14 +0100

    Lokal1 ve Lokal2 dosyalari güncellendi

--- src/Local1.java
+++ src/Local1.java
@@ -3,5 +3,7 @@ public class Local1 {
         System.out.println("Git Test");
         System.out.println("gityeni satir eklendi!");
         System.out.println("yeni test");
+        System.out.println("cuma");
+        System.out.println("Fatma branch in da eklenen code!!");
     }
 }
