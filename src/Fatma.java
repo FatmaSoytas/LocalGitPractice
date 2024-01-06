041a390daf4053b92ed74f7a7e5b928294b5afca
FatmaSoytas <146882441+FatmaSoytas@users.noreply.github.com>
2024-01-06 18:58:35 +0100

    Remote da Fatma dosyasi guncellendi

--- src/Fatma.java
+++ src/Fatma.java
@@ -1,5 +1,7 @@
 public class Fatma {
     public static void main(String[] args) {
-        System.out.println("Fatma branch in da eklenen code!!");
+        System.out.println("Fatma branch in da eklenen code!! burayida degistirdim  ");
+        //Yeni satirlar eklendi
+          System.out.println("Fatma branch ina GITUP REMOTE dan eklemeler yaptim..!!");
     }
 }
