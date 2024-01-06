03c7a3b853b1c29d9e3196072498a8b454d787da
Fatma SOYTAS <fatmasoytas81@homail.com>
2024-01-06 13:32:00 +0100

    Local1 ve Local2 eklendi

---
+++ src/Local2.java
@@ -0,0 +1,8 @@
+public class Local2 {
+    public static void main(String[] args) {
+        System.out.println("Test1");
+        System.out.println("Test2");
+        System.out.println("cuma");
+        System.out.println("Fatma branch in da eklenen code!!");
+    }
+}
