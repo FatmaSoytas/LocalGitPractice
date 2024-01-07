73288b2db0e647d61e137105dc8eba8c2237a6f1
Fatma SOYTAS <fatmasoytas81@homail.com>
2024-01-08 00:28:52 +0100

    Main i yeniledim

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,8 @@
         public static void main(String[] args) {
 
             System.out.println("Proje mi Baslat !!");
+            // Yenilik
+            System.out.println("Projemi yeniledim !!");
         }
     }
 
